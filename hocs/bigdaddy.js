6e926ebdf9c89c44b0d688a0b0933df6b5948303
vthang95 <tranvietthang95@gmail.com>
2017-12-11 21:49:29 +0700

    init project

---
+++ hocs/bigdaddy.js
@@ -0,0 +1,43 @@
+import { Component } from 'react'
+import Head from 'next/head'
+import vi from 'antd/lib/locale-provider/en_US';
+import { LocaleProvider } from 'antd'
+
+import MainLayout from 'layouts/main.layout'
+
+const bigdaddy = OurChildComponent => {
+  class HigherOrderComponent extends Component {
+    static async getInitialProps(ctx) {
+      // Do something in serverside here
+      const childProps = OurChildComponent.getInitialProps ? await OurChildComponent.getInitialProps(ctx) : {};
+
+      return { ...childProps }
+    }
+
+    render() {
+      return (
+        <div>
+          <Head>
+            <meta charSet="utf-8" />
+            <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <title>Simple Nextjs Boilerplate!</title>
+            <meta name="description" content="A boilerplate for server side rendering with react and nextjs" />
+
+            <link href="/static/css/antd.min.2.13.9.css" rel="stylesheet" />
+            <link href="/static/dist/global.css" rel="stylesheet" />
+          </Head>
+          <LocaleProvider locale={vi}>
+            <MainLayout>
+              <OurChildComponent { ...this.props } />
+            </MainLayout>
+          </LocaleProvider>
+        </div>
+      )
+    }
+  }
+
+  return HigherOrderComponent
+}
+
+export default bigdaddy
---
+++ pages/index.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Button } from 'antd'
+
+import Notification from 'components/Notification'
+import bigdaddy from 'hocs/bigdaddy'
+import style from './index.scss'
+import { authGetInfo } from 'actions'
+import connect from 'store'
+
+class Index extends React.Component {
+  static async getInitialProps(ctx) {
+    // do something in server here!
+    const { store } = ctx
+
+    const sendString = "I Got You in Server! Dont believe? You can view page source and find me there!..."
+    store.dispatch(authGetInfo(sendString))
+
+    return {}
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="hero">
+          <div className="hero-body">
+            <h1>Welcome to the boilerplate!</h1>
+            <h3>{this.props.auth.info}</h3>
+            <div>
+              <Button
+                type="danger"
+                style={{ marginTop: 20 }}
+                onClick={() => Notification.success('Yay!', 'You clicked!')}
+              >
+                Click Me!
+              </Button>
+            </div>
+          </div>
+        </div>
+        <style dangerouslySetInnerHTML={{ __html: style }} />
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    auth: state.auth
+  }
+}
+
+export default connect(mapStateToProps, { authGetInfo })(bigdaddy(Index))
