b961d8b743a3df1eac8927a599d8284469ede9bf
khoabeo <khoatv141001@gmail.com>
2017-12-11 22:43:02 +0700

    delete xl content

--- hocs/bigdaddy.js
+++ hocs/bigdaddy.js
@@ -8,9 +8,7 @@ import MainLayout from 'layouts/main.layout'
 const bigdaddy = OurChildComponent => {
   class HigherOrderComponent extends Component {
     static async getInitialProps(ctx) {
-      // Do something in serverside here
       const childProps = OurChildComponent.getInitialProps ? await OurChildComponent.getInitialProps(ctx) : {};
-
       return { ...childProps }
     }
 
@@ -21,9 +19,7 @@ const bigdaddy = OurChildComponent => {
             <meta charSet="utf-8" />
             <meta httpEquiv="x-ua-compatible" content="ie=edge" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <title>Simple Nextjs Boilerplate!</title>
-            <meta name="description" content="A boilerplate for server side rendering with react and nextjs" />
-
+            <title>{OurChildComponent.info.title}</title>
             <link href="/static/css/antd.min.2.13.9.css" rel="stylesheet" />
             <link href="/static/dist/global.css" rel="stylesheet" />
           </Head>
@@ -32,6 +28,7 @@ const bigdaddy = OurChildComponent => {
               <OurChildComponent { ...this.props } />
             </MainLayout>
           </LocaleProvider>
+          <style dangerouslySetInnerHTML={{ __html: OurChildComponent.info.style }} />
         </div>
       )
     }
--- pages/index.js
+++ pages/index.js
@@ -4,39 +4,17 @@ import { Button } from 'antd'
 import Notification from 'components/Notification'
 import bigdaddy from 'hocs/bigdaddy'
 import style from './index.scss'
-import { authGetInfo } from 'actions'
-import connect from 'store'
 
 class Index extends React.Component {
-  static async getInitialProps(ctx) {
-    // do something in server here!
-    const { store } = ctx
 
-    const sendString = "I Got You in Server! Dont believe? You can view page source and find me there!..."
-    store.dispatch(authGetInfo(sendString))
-
-    return {}
+  static info = {
+    title: 'Home',
+    style
   }
 
   render() {
     return (
       <div>
-        <div className="hero">
-          <div className="hero-body">
-            <h1>Welcome to the boilerplate!</h1>
-            <h3>{this.props.auth.info}</h3>
-            <div>
-              <Button
-                type="danger"
-                style={{ marginTop: 20 }}
-                onClick={() => Notification.success('Yay!', 'You clicked!')}
-              >
-                Click Me!
-              </Button>
-            </div>
-          </div>
-        </div>
-        <style dangerouslySetInnerHTML={{ __html: style }} />
       </div>
     )
   }
@@ -48,4 +26,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps, { authGetInfo })(bigdaddy(Index))
+export default bigdaddy(Index)
